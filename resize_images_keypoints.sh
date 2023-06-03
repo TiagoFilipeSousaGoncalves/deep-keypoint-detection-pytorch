e757b008e9900367c6e2bd3a1bbe3a040d3d2c2e
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2023-06-03 16:08:06 +0100

    Updated: resize_images_keypoints.sh

--- resize_images_keypoints.sh
+++ resize_images_keypoints.sh
@@ -2,4 +2,4 @@
 
 
 
-python code/resize_images_keypoints.py --database picture-db --original_path /nas-ctm01/datasets/private/CINDERELLA/picture-db --new_path /nas-ctm01/datasets/private/CINDERELLA/processed-databases/deep-keypoint-detection
+python code/resize_images_keypoints.py --database picture-db --original_path /nas-ctm01/datasets/private/CINDERELLA/picture-db --new_path /nas-ctm01/datasets/private/CINDERELLA/processed-databases/deep-keypoint-detection 
