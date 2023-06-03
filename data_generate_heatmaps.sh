1ab6352c4e9d90176141f320de5de385513f2a8b
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2023-06-03 16:30:17 +0100

    Deleted: resize_images_keypoints.sh; updated: data_generate_heatmaps.sh, data_resize_images_keypoints.sh

---
+++ data_generate_heatmaps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+
+python code/data_generate_heatmaps.py --database picture-db
--- resize_images_keypoints.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-
-python code/data_resize_images_keypoints.py --database picture-db --original_path /nas-ctm01/datasets/private/CINDERELLA/picture-db --new_path /nas-ctm01/datasets/private/CINDERELLA/processed-databases/deep-keypoint-detection 
