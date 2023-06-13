dccfe3d735e661755c4436b3f160a756079c3e06
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2023-06-13 19:27:56 +0100

    Updated: train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH -p gtx1080ti_11GB              # Partition
-#SBATCH --qos=gtx1080ti                # QOS
+#SBATCH -p v100_32GB              # Partition
+#SBATCH --qos=v100                # QOS
 #SBATCH --job-name=cinderella_dkd      # Job name
 #SBATCH -o slurm.%N.%j.out             # STDOUT
 #SBATCH -e slurm.%N.%j.err             # STDERR
@@ -10,6 +10,6 @@
 
 echo "CINDERELLA Deep Keypoint Detection | PICTURE-DB | Started"
 
-python code/train.py --database picture-db --epochs 300 --batch_size 8 --num_workers 4 --gpu_id 0 --results_directory results
+python code/train.py --database picture-db --epochs 300 --batch_size 16 --num_workers 4 --gpu_id 0 --results_directory results
 
 echo "CINDERELLA Deep Keypoint Detection | PICTURE-DB | Finished"
